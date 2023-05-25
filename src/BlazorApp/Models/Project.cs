0b5e267ca7ffec54d11c9007e604919f36f9dacd
vzades <vinay.komal100@gmail.com>
2023-05-25 21:57:27 +0530

    added new images

--- src/BlazorApp/Models/Project.cs
+++ src/BlazorApp/Models/Project.cs
@@ -5,4 +5,6 @@ public class Project
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
+
+    public string Img { get; set; } = string.Empty;
 }
